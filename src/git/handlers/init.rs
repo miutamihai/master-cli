acac8d41c00f9a65f006502404114d4d1f040b4e
miutamihai <miuta.mihai@gmail.com>
2023-07-13 00:01:35 +0300

    [master] Add check for workdir

--- src/git/handlers/init.rs
+++ src/git/handlers/init.rs
@@ -1,3 +1,21 @@
-pub fn init() {
-    println!("Running git init")
+use std::env;
+
+fn is_work_dir() -> bool {
+    let work_dir = "sevencode";
+
+    match env::current_dir() {
+        Ok(current_dir) => {
+            current_dir.components()
+                .any(|value| value.as_os_str().eq(work_dir))
+        }
+        Err(_) => { false }
+    }
+}
+
+pub fn init() -> Option<()> {
+    println!("Running git init");
+
+    println!("is work dir {}", is_work_dir());
+
+    None
 }
