a7f0c3f31cb9a6c3dfa747efafc1045c5c514630
miutamihai <miuta.mihai@gmail.com>
2023-07-12 22:10:36 +0300

    [master] Read details from cargo file

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(name = "mm")]
-#[command(author = "Mihai Miuta <miuta.mihai@gmail.com>")]
-#[command(version = "0.0.1")]
-#[command(about = "CLI to handle common tasks", long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub struct Args {
     #[arg(short, long)]
     pub name: String,
