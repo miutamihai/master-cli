73178d23aec0810d3e1aa0a2f35680faf1b7013e
miutamihai <miuta.mihai@gmail.com>
2023-07-13 00:38:19 +0300

    [master] Implement repo initialization

--- src/git/handlers/init.rs
+++ src/git/handlers/init.rs
@@ -1,5 +1,6 @@
 use std::env;
-use log::info;
+use log::{error, info};
+use std::process::{Command, exit};
 
 fn is_work_dir() -> bool {
     let work_dir = "sevencode";
@@ -13,12 +14,58 @@ fn is_work_dir() -> bool {
     }
 }
 
+fn initialize_repo() {
+   match Command::new("git")
+       .arg("init")
+       .spawn() {
+       Ok(_) => {}
+       Err(_) => {
+           error!("Failed to initialize git repository!");
+           exit(1)
+       }
+   }
+}
+
+fn git_config(key: &str, value: &str) {
+    match Command::new("git")
+        .arg("config")
+        .arg(key)
+        .arg(value)
+        .spawn() {
+        Ok(_) => {}
+        Err(_) => {
+            error!("Failed to set git config {}!", key);
+            exit(1)
+        }
+    }
+}
+
+struct GitConfig {
+    name: &'static str,
+    email: &'static str,
+}
+
 pub fn init() {
     info!("Initializing git repository...");
+    initialize_repo();
+
+    let work_config = GitConfig {
+        name: "mihaisevencode",
+        email: "mihai.miuta@7code.ro"
+    };
+
+    let personal_config = GitConfig {
+        name: "miutamihai",
+        email: "miuta.mihai@gmail.com"
+    };
 
     if is_work_dir() {
-        info!("Using work credentials")
+        info!("Using work credentials");
+        git_config("user.name", work_config.name);
+        git_config("user.email", work_config.email);
     } else {
-        info!("Using personal credentials")
+        info!("Using personal credentials");
+        git_config("user.name", personal_config.name);
+        git_config("user.email", personal_config.email);
     }
 }
