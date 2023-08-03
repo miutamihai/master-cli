10b2728c048b115000399e50ac64524dccd97bb5
miutamihai <miuta.mihai@gmail.com>
2023-08-03 10:11:24 +0300

    [master] Remove more dead code

--- src/git/handlers/restart/mod.rs
+++ src/git/handlers/restart/mod.rs
@@ -4,7 +4,6 @@ use crate::git::handlers::restart::get_commands::get_commands;
 mod build_input;
 mod get_args;
 mod get_commands;
-mod message;
 
 pub fn restart(destination: &String, origin: &String) {
     get_commands(destination, origin)
