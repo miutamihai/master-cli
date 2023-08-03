305dabebb32701aa42e93d422248e47e8cf54950
miutamihai <miuta.mihai@gmail.com>
2023-08-03 12:27:05 +0300

    [master] Correct install script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone git@gitlab.com:miutamihai/master-maker-cli.git mm
 cd mm || exit
 
 cargo build -r
-sudo mv ./target/release/master_maker /usr/local/bin/mm
+sudo mv ./target/release/mm /usr/local/bin/mm
 
 cd ..
 rm -rf mm
