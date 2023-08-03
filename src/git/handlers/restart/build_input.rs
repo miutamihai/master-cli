0a0b5a431669e9a7ecfe84b8b4b6dabe7555ecd2
miutamihai <miuta.mihai@gmail.com>
2023-08-03 10:52:47 +0300

    [master] Add test for build_input

--- src/git/handlers/restart/build_input.rs
+++ src/git/handlers/restart/build_input.rs
@@ -8,3 +8,35 @@ pub fn build_input(args: Vec<String>) -> Input {
         on_error: None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::common::run::Input;
+    use crate::git::handlers::restart::build_input::build_input;
+
+    fn get_args() -> Vec<String> {
+        vec!["checkout", "master"]
+            .iter()
+            .map(|arg| {String::from(*arg)})
+            .collect()
+    }
+
+    #[test]
+    fn build_input_test() {
+        let args = get_args();
+        let expected = Input {
+            cmd: String::from("git"),
+            args: args.clone(),
+            on_done: None,
+            on_error: None
+        };
+
+        let actual = build_input(args);
+
+        assert_eq!(expected.cmd, actual.cmd);
+        assert_eq!(expected.args, actual.args);
+        assert_eq!(expected.on_done, actual.on_done);
+        assert_eq!(expected.on_error, actual.on_error);
+    }
+
+}
