851f8bd036e71900e4e5aeeaedb85bcf0f527277
miutamihai <miuta.mihai@gmail.com>
2023-08-10 11:32:20 +0300

    [master] Add config editor

--- src/config/handle/in_editor.rs
+++ src/config/handle/in_editor.rs
@@ -1,5 +1,13 @@
+use crate::common::exit_with_errors::exit_with_errors;
+use crate::config::config_path::config_path;
+use edit::edit_file;
 use log::info;
 
 pub fn in_editor() {
-    info!("Editting...")
+    match edit_file(config_path()) {
+        Ok(_) => {
+            info!("Config updated!")
+        }
+        Err(_) => exit_with_errors("Failed to edit config"),
+    }
 }
--- src/config/handle/mod.rs
+++ src/config/handle/mod.rs
@@ -3,26 +3,21 @@ use crate::config::handle::in_editor::in_editor;
 use crate::config::handle::inline::inline;
 use crate::config::model::Config;
 
-mod inline;
 mod in_editor;
+mod inline;
 
 fn build_message(missing: &'static str, existing: &'static str) -> String {
-    format!("Argument `--{}` is required when `--{}` is passed", missing, existing)
+    format!(
+        "Argument `--{}` is required when `--{}` is passed",
+        missing, existing
+    )
 }
 
 pub fn handle(name_option: &Option<String>, value_option: &Option<String>, config: Config) {
     match (name_option, value_option) {
-        (None, None) => {
-            in_editor()
-        }
-        (Some(name), Some(value)) => {
-            inline(name, value, config)
-        }
-        (None, Some(_)) => {
-            exit_with_errors(build_message("name", "value"))
-        }
-        (Some(_), None) => {
-            exit_with_errors(build_message("value", "name"))
-        }
+        (None, None) => in_editor(),
+        (Some(name), Some(value)) => inline(name, value, config),
+        (None, Some(_)) => exit_with_errors(build_message("name", "value")),
+        (Some(_), None) => exit_with_errors(build_message("value", "name")),
     }
 }
