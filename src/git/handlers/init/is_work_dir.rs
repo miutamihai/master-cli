23c94346654002f277ad770ba8201a683e3bb4b9
miutamihai <miuta.mihai@gmail.com>
2023-08-10 11:43:16 +0300

    [master] Allow only absolute paths for work_dir

--- src/git/handlers/init/is_work_dir.rs
+++ src/git/handlers/init/is_work_dir.rs
@@ -1,17 +1,25 @@
+use std::env;
+use std::fs::canonicalize;
+
 use crate::common::exit_with_errors::exit_with_errors;
 use crate::config::model::Config;
-use std::env;
-use std::path::Path;
 
 pub fn is_work_dir(config: &Config) -> bool {
-    let work_dir_path = Path::new(&config.git.work_dir);
+    match canonicalize(&config.git.work_dir) {
+        Ok(path) => {
+            let work_dir_path = path.as_path();
 
-    if !work_dir_path.exists() {
-        exit_with_errors("Current `git.work_dir` path doesn't exist")
-    }
+            if !work_dir_path.exists() {
+                exit_with_errors("Current `git.work_dir` path doesn't exist")
+            }
 
-    match env::current_dir() {
-        Ok(current_dir) => work_dir_path.eq(&current_dir),
-        Err(_) => false,
+            match env::current_dir() {
+                Ok(current_dir) => work_dir_path.eq(&current_dir),
+                Err(_) => false,
+            }
+        }
+        Err(_) => {
+            exit_with_errors("Current `git.work_dir` path is invalid or not an absolute path")
+        }
     }
 }
