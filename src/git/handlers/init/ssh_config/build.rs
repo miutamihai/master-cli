eecf16ec2f6dbd5eb852e6b195ccab9d890318e8
miutamihai <miuta.mihai@gmail.com>
2023-08-16 18:14:30 +0300

    [master] Use canonical_path for key_path

--- src/git/handlers/init/ssh_config/build.rs
+++ src/git/handlers/init/ssh_config/build.rs
@@ -1,12 +1,22 @@
-use crate::common::exit_with_errors::exit_with_errors;
+use std::fs::canonicalize;
+
 use string_builder::Builder;
+
+use crate::common::exit_with_errors::exit_with_errors;
 
 pub fn build(key_path: &String) -> String {
     let mut builder = Builder::default();
+    let canonical_path = match canonicalize(key_path) {
+        Ok(path) => path,
+        Err(_) => exit_with_errors(format!("Key path {} is invalid", key_path)),
+    };
 
     builder.append(format!("Host {}\n", key_path));
     builder.append(format!("\tUser git\n"));
-    builder.append(format!("\tIdentityFile {}\n", key_path));
+    builder.append(format!(
+        "\tIdentityFile {}\n",
+        canonical_path.to_str().unwrap()
+    ));
     builder.append(format!("\tIdentitiesOnly yes\n"));
 
     match builder.string() {
